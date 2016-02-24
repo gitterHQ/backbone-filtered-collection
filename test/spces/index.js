8dc46b256ae373df3b295e56506ae92e7e43ebf0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-02-24 11:48:25 +0000

    initial test setup

---
+++ test/spces/index.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('A test', function(){
+  it('should pass', function(){
+    console.log('-----------------------');
+    console.log(document.body.classList);
+    console.log('-----------------------');
+    assert(true);
+  });
+});
